47ccb216b141e57e1a23b9cc0e7a5fa6aec8dc15
Omnia8888 <omnia8888@gmail.com>
2015-10-09 13:45:51 +0200

    First commit

---
+++ MickeConfigSettings/MickeConfigSettings.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MickeConfigSettings.ino
+ Created:	10/9/2015 1:44:48 PM
+ Author:	Micke
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
