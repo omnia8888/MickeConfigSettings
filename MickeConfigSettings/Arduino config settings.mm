3ec3be6a0dd98dc1c8bb49c637e9def804b8ce59
Omnia8888 <omnia8888@gmail.com>
2015-10-09 14:08:27 +0200

    Added Project Mind Map

---
+++ MickeConfigSettings/Arduino config settings.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1444391248593" ID="ID_604503619" MODIFIED="1444391367432" TEXT="Arduino config settings">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1444391336866" ID="ID_1925255732" MODIFIED="1444391372496" POSITION="right" STYLE="bubble" TEXT="Stored in EEprom">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1444391396755" ID="ID_758276335" MODIFIED="1444391426974" TEXT="Save Data">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
